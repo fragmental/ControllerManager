6b3b0990fd854d699b1f8b4bbaf70b398a7d2408
Stuart Nelson <stuart.blake.nelson@gmail.com>
2016-12-10 13:06:53 -0700

    generalizing functions

--- ControllerManager.cs
+++ ControllerManager.cs
@@ -10,24 +10,6 @@ public class ControllerManager{
         LIN
     };
 
-	public bool Player1Keyboard {
-		get{ return i.player1Keyboard;}
-		set{ 
-			i.player1Keyboard = value; 
-            //if (i.player1Keyboard) {
-            //    i.player2Keyboard = false;
-            //}
-		}
-	}
-	public bool Player2Keyboard {
-		get{ return i.player2Keyboard;}
-		set{
-			i.player2Keyboard = value;
-            //if (i.player2Keyboard) {
-            //    i.player1Keyboard = false;
-            //}
-		}
-	}
 	public static ControllerManager Instance {
 		get {
 			if (i == null) {
@@ -35,9 +17,13 @@ public class ControllerManager{
 				if (Application.platform == RuntimePlatform.OSXEditor
 					|| Application.platform == RuntimePlatform.OSXPlayer) {
 					i.OS = OSX;
-				} else (Application.platform == RuntimePlatform.OSXEditor
-					|| Application.platform == RuntimePlatform.OSXPlayer){
-					i.osx = false;
+				} else  if (Application.platform == RuntimePlatform.WindowsEditor
+					|| Application.platform == RuntimePlatform.WindowsPlayer){
+					i.OS = WIN;
+				} else  if (Application.platform == RuntimePlatform.LinuxPlayer{
+					i.OS = LIN;
+				} else {
+					throw new Exception(); // Unsupported platform
 				}
 			}
 			return i;
@@ -45,154 +31,25 @@ public class ControllerManager{
 	}
 
 	private static ControllerManager i;
-	private bool player1Keyboard = true;
-	private bool player2Keyboard;
-	private bool osx;
-    private bool isAxisInUse  = false;
 
 
 	/// <summary>
-	/// Gets the player movement on the x axis.
+	/// Gets the left joystick x axis.
 	/// </summary>
-	/// <returns>The raw axis value for the x axis. -1, 0, or 1</returns>
+	/// <returns>The axis value for the x axis. -1 to 1</returns>
 	/// <param name="player">Player number ( 1 or 2 )</param>
-	public float GetMovementX(int player)
+	public float GetLeftJoystickX(int player)
 	{
-		if (player == 1) {
-
-			if (i.Player1Keyboard) {
-				//get player 1 keyboard movement
-				//Debug.Log ("keyboard");
-				return Input.GetAxisRaw ("Horizontal");
-			} else {
-				//get player 1 controller movement
-				return Input.GetAxisRaw("x-axis1");
-			}
-		} else {
-			if (i.Player2Keyboard) {
-				//get player 2 keyboard movement
-				//Debug.Log ("keyboard player2");
-				return Input.GetAxisRaw ("Horizontal");
-			} else {
-				//get player 2 controller movement
-                Debug.Log("controller player2");
-                
-				return Input.GetAxisRaw("x-axis2");
-			}
-		}
+		return Input.GetAxis("x-axis" + player);
 	}
 	/// <summary>
-	/// Gets the player movement on the y axis.
+	/// Gets the left joystick y axis.
 	/// </summary>
-	/// <returns>The raw axis value for the y axis. -1, 0, or 1</returns>
+	/// <returns>The axis value for the y axis. -1 to 1</returns>
 	/// <param name="player">Player number ( 1 or 2 )</param>
-	public float GetMovementY(int player)
+	public float GetLeftJoystickY(int player)
 	{
-		if (player == 1) {
-			if (i.Player1Keyboard) {
-				//get player 1 keyboard movement
-				return Input.GetAxisRaw ("Vertical");
-			} else {
-				//get player 1 controller movement
-				return -Input.GetAxisRaw("y-axis1");
-			}
-		} else {
-			if (i.Player2Keyboard) {
-				//get player 2 keyboard movement
-				return Input.GetAxisRaw ("Vertical");
-			} else {
-				//get player 2 controller movement
-				return -Input.GetAxisRaw("y-axis2");
-			}
-		}
-	}
-	/// <summary>
-	/// Gets the angle of direction from mouse or right joystick.
-	/// </summary>
-	/// <returns>The direction angle in radians.</returns>
-	/// <param name="player">Player number.</param>
-	/// <param name="screenLocation">Screen location of object.</param>
-	public float GetDirectionAngle(int player, Vector3 screenLocation = new Vector3())
-	{
-		float angle;
-		if (player == 1) {
-			if (i.Player1Keyboard) {
-				//get player 1 mouse direction angle
-				Vector3 mousePos = Input.mousePosition;
-				mousePos.x = mousePos.x - screenLocation.x;
-				mousePos.y = mousePos.y - screenLocation.y;
-				angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-				return angle;
-			} else {
-				//get player 1 controler direction angle
-				angle = Mathf.Atan2(-i.GetRightJoystickY (player), 
-					i.GetRightJoystickX (player)) * Mathf.Rad2Deg;
-				return angle;
-			}
-		} else {
-			if (i.Player2Keyboard) {
-				//get player 2 mouse direction angle
-				Vector3 mousePos = Input.mousePosition;
-				mousePos.x = mousePos.x - screenLocation.x;
-				mousePos.y = mousePos.y - screenLocation.y;
-				angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-				return angle;
-			} else {
-				//get player 2 controller direction angle
-				angle = Mathf.Atan2(-i.GetRightJoystickY (player), 
-					i.GetRightJoystickX (player)) * Mathf.Rad2Deg;
-				return angle;
-			}
-		}
-	}
-	/// <summary>
-	/// Returns which, if any, button the player is selecting.
-	/// left trigger == 1, left bumper == 2, right bumper == 3. If none of these buttons are selected, return 0
-	/// </summary>
-	/// <param name="player">Player number</param>
-	public int Switch(int player){
-		int value = 0;
-		if ((player == 1 && i.Player1Keyboard)
-			|| (player == 2 && i.Player2Keyboard)) {
-			if (Input.GetKeyDown (KeyCode.Alpha1))
-				value = 1;
-			else if (Input.GetKeyDown (KeyCode.Alpha2))
-				value = 2;
-			else if (Input.GetKeyDown (KeyCode.Alpha3))
-				value = 3;
-			else
-				value = 0;
-		} else {
-			if (i.GetLeftTrigger (player) > 0)
-				value = 1;
-			else if (i.GetLeftBumper (player))
-				value = 2;
-			else if (i.GetRightBumper (player))
-				value = 3;
-			else
-				value = 0;
-		}
-		if (value != 0) {
-			i.WeaponChangedCount++;
-		}
-		return value;
-	}
-    public bool Pause(int player)
-    {
-        if ((player == 1 && i.Player1Keyboard)
-            || (player == 2 && i.Player2Keyboard))
-            return Input.GetKeyDown(KeyCode.Escape);
-        else
-            return i.GetStartButton(player);
-    }
-
-
-	public bool Fire(int player){
-		if ((player == 1 && i.Player1Keyboard)
-		    || (player == 2 && i.Player2Keyboard))
-			return Input.GetMouseButtonDown (0);
-		else
-			return GetRightTrigger (player) > 0;
+		return Input.GetAxis("y-axis" + player);
 	}
 
 	public float GetRightJoystickX(int player)
@@ -211,26 +68,20 @@ public class ControllerManager{
 			return Input.GetAxis("5th-axis" + player);
 		}
 	}
+
+	/// <summary>
+	/// Gets the right trigger axis as a value from 0 (not engaged) to 1 (fully engaged).
+	/// </summary>
+	/// <returns>The axis value for the right trigger axis. 0 ro 1</returns>
+	/// <param name="player">Player number ( 1 or 2 )</param>
 	public float GetRightTrigger(int player)
 	{
 		if (osx) {
-			return Input.GetAxis ("6th-axis" + player);
+			// Convert -1 - 1 range to 0 - 1 range
+			float value = Input.GetAxis ("6th-axis" + player);
+			return convertRange(-1f, 1f, 0f, 1f, value);
 		} else {
-			//return Input.GetAxis ("10th-axis" + player);
-            if (Input.GetAxis("10th-axis" + player) != 0)
-            {
-                if (isAxisInUse == false)
-                {
-                    isAxisInUse = true;
-                    return Input.GetAxis("10th-axis" + player);
-                    
-                }
-            }
-            if (Input.GetAxis("10th-axis" + player) == 0)
-            {
-                isAxisInUse = false;
-            }
-            return 0;
+			return Input.GetAxis ("10th-axis" + player);
         }
 
 	}
@@ -337,5 +188,12 @@ public class ControllerManager{
 			else
 				return Input.GetKeyDown (KeyCode.Joystick2Button1);
 		}
+	}
+	private float convertRange(float originalStart, float originalEnd,
+    						   float newStart, float newEnd,
+    						   float value){
+
+	    float scale = (newEnd - newStart) / (originalEnd - originalStart);
+	    return (newStart + ((value - originalStart) * scale));
 	}
 }
--- README.rst
+++ README.rst
@@ -1,4 +1,8 @@
 ControllerManager
 =================
 
-ControllerManager is a cross platform multiplayer controller manager for Unity. ControllerManager maps up to 4 XBox 360/XBOne controllers. Currently works in macOS, Windows, and Linux(untested).
+ControllerManager is a cross platform multiplayer controller manager for Unity. ControllerManager maps up to 4 XBox 360/XBOne controllers. Currently works in macOS and Windows. Linux coming soon. 
+
+Currently works for two xboxs controllers.
+
+Unity input manager must include the axes as specified in the InputManager.asset file. 
